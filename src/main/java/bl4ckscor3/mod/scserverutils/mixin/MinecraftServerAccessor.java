7b9e29aa8a92e99cf6d09e8307b4869f1817a565
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-04-24 20:40:18 +0200

    readd accidentally removed accessor annotation

--- src/main/java/bl4ckscor3/mod/scserverutils/mixin/MinecraftServerAccessor.java
+++ src/main/java/bl4ckscor3/mod/scserverutils/mixin/MinecraftServerAccessor.java
@@ -1,10 +1,12 @@
 package bl4ckscor3.mod.scserverutils.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
 import net.minecraft.server.MinecraftServer;
 
 @Mixin(MinecraftServer.class)
 public interface MinecraftServerAccessor {
+	@Accessor
 	public void setTicksUntilAutosave(int ticksUntilAutosave);
 }
