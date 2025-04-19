b78b0fc250f3fcb872f74ec08f98a07411d0fcc9
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-04-19 21:55:57 +0200

    Fix rift stabilizer config options being flipped

--- src/main/java/bl4ckscor3/mod/scserverutils/configuration/SpawnProtectionRiftStabilizer.java
+++ src/main/java/bl4ckscor3/mod/scserverutils/configuration/SpawnProtectionRiftStabilizer.java
@@ -6,4 +6,4 @@ import net.neoforged.neoforge.common.ModConfigSpec.BooleanValue;
 import net.neoforged.neoforge.common.ModConfigSpec.ConfigValue;
 import net.neoforged.neoforge.common.ModConfigSpec.IntValue;
 
-public record SpawnProtectionRiftStabilizer(BooleanValue enabled, ConfigValue<List<? extends String>> disallowedTeleportationTypesToSpawn, ConfigValue<List<? extends String>> disallowedTeleportationTypesFromSpawn, IntValue bypassPermissionLevel, ConfigValue<? extends String> langKey, ConfigValue<? extends String> fallback) {}
+public record SpawnProtectionRiftStabilizer(BooleanValue enabled, ConfigValue<List<? extends String>> disallowedTeleportationTypesFromSpawn, ConfigValue<List<? extends String>> disallowedTeleportationTypesToSpawn, IntValue bypassPermissionLevel, ConfigValue<? extends String> langKey, ConfigValue<? extends String> fallback) {}
