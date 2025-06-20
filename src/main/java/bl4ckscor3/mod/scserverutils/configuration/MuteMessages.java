6cba02a88b150f7ec57d50a23cb3961c5e78a055
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2025-06-20 10:09:25 +0200

    Update MuteMessages.java

--- src/main/java/bl4ckscor3/mod/scserverutils/configuration/MuteMessages.java
+++ src/main/java/bl4ckscor3/mod/scserverutils/configuration/MuteMessages.java
@@ -2,4 +2,12 @@ package bl4ckscor3.mod.scserverutils.configuration;
 
 import net.neoforged.neoforge.common.ModConfigSpec;
 
-public record MuteMessages(ModConfigSpec.ConfigValue<String> enter, ModConfigSpec.ConfigValue<String> leave) {}
+public record MuteMessages(ModConfigSpec.ConfigValue<String> cancelledMessageMute, 
+ModConfigSpec.ConfigValue<String> cancelledMessageSuspend, 
+ModConfigSpec.ConfigValue<String> muteStarts,
+ModConfigSpec.ConfigValue<String> muteEnds,
+ModConfigSpec.ConfigValue<String> muteFailedAlreadyMuted,
+ModConfigSpec.ConfigValue<String> muteSuccess,
+ModConfigSpec.ConfigValue<String> unmuteFailedNotMuted,
+ModConfigSpec.ConfigValue<String> unmuteSuccess
+) {}
