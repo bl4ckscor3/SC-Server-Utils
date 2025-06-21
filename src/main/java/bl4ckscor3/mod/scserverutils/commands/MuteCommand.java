65b612ac4cf93c302b4f6943d0edfac52a4c13ad
LiveinGround <129398571+LiveInGround@users.noreply.github.com>
2025-06-21 10:15:54 +0200

    [Fix] Messages now parsed correctly

--- src/main/java/bl4ckscor3/mod/scserverutils/commands/MuteCommand.java
+++ src/main/java/bl4ckscor3/mod/scserverutils/commands/MuteCommand.java
@@ -4,13 +4,17 @@ import bl4ckscor3.mod.scserverutils.SCServerUtils;
 import bl4ckscor3.mod.scserverutils.configuration.Configuration;
 import bl4ckscor3.mod.scserverutils.configuration.MuteMessages;
 import bl4ckscor3.mod.scserverutils.mute.PlayerMuteData;
+import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.commands.arguments.GameProfileArgument;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.network.chat.Component;
+
+import java.util.Collection;
 
 public class MuteCommand {
 
@@ -34,10 +38,10 @@ public class MuteCommand {
                             String reason = StringArgumentType.getString(ctx, "reason");
                             SCServerUtils.playerDataManager.addEntry(new PlayerMuteData(playerToMute.getName().toString(), playerToMute.getStringUUID(), reason));
 
-                            Component messageComponent = Component.translatable("scserverutils.mute.mute_starts", reason);
+                            Component messageComponent = SCServerUtils.parseComponent(playerToMute.level(), String.format(muteMessages.muteStarts().get(), reason));
                             playerToMute.sendSystemMessage(messageComponent);
 
-                            Component messageToOperator = Component.translatable("scserverutils.mute.success", playerToMute.getName().toString());
+                            Component messageToOperator = SCServerUtils.parseComponent(ctx.getSource().getLevel(), String.format(muteMessages.muteSuccess().get(), playerToMute.getName()));
                             ctx.getSource().sendSuccess(() -> messageToOperator, true);
                             return 1;
                         }
@@ -45,27 +49,36 @@ public class MuteCommand {
 
         dispatcher.register(Commands.literal("unmute")
                 .requires(source -> source.hasPermission(permissionLevel))
-                .then(Commands.argument("player", EntityArgument.player())
+                .then(Commands.argument("player", GameProfileArgument.gameProfile())
                         .executes(ctx -> {
 
                             MuteMessages muteMessages = Configuration.instance.muteMessages;
 
-                            ServerPlayer playerToUnmute = EntityArgument.getPlayer(ctx, "player");
+                            Collection<GameProfile> profiles = GameProfileArgument.getGameProfiles(ctx, "player");
+                            if (!profiles.isEmpty()) {
 
-                            if (!SCServerUtils.mutedPlayersUUID.contains(playerToUnmute.getStringUUID())) {
-                                Component messageToOperator = SCServerUtils.parseComponent(ctx.getSource().getPlayerOrException().level(), muteMessages.unmuteFailedNotMuted().get());
-                                ctx.getSource().sendFailure(messageToOperator);
+                                GameProfile targetProfile = profiles.iterator().next();
+                                ServerPlayer playerToUnmute = ctx.getSource().getServer().getPlayerList().getPlayer(targetProfile.getId());
+
+
+                                if (!SCServerUtils.mutedPlayersUUID.contains(playerToUnmute.getStringUUID())) {
+                                    Component messageToOperator = SCServerUtils.parseComponent(ctx.getSource().getPlayerOrException().level(), muteMessages.unmuteFailedNotMuted().get());
+                                    ctx.getSource().sendFailure(messageToOperator);
+                                    return 1;
+                                }
+                                SCServerUtils.playerDataManager.removeEntry(SCServerUtils.playerDataManager.getPlayerDataByUUID(playerToUnmute.getStringUUID()));
+
+
+                                Component messageComponent = SCServerUtils.parseComponent(playerToUnmute.level(), muteMessages.muteEnds().get());
+                                playerToUnmute.sendSystemMessage(messageComponent);
+
+                                Component messageToOperator = SCServerUtils.parseComponent(ctx.getSource().getLevel(), String.format(muteMessages.unmuteSuccess().get(), playerToUnmute.getName()));
+                                ctx.getSource().sendSuccess(() -> messageToOperator, true);
+                                return 1;
+                            } else {
+                                ctx.getSource().sendFailure(Component.literal("No player with this username was found."));
                                 return 1;
                             }
-                            SCServerUtils.playerDataManager.removeEntry(SCServerUtils.playerDataManager.getPlayerDataByUUID(playerToUnmute.getStringUUID()));
-
-
-                            Component messageComponent = SCServerUtils.parseComponent(playerToUnmute.level(), muteMessages.muteEnds().get());
-                            playerToUnmute.sendSystemMessage(messageComponent);
-
-                            Component messageToOperator = Component.translatable("scserverutils.unmute.success", playerToUnmute.getName().toString());
-                            ctx.getSource().sendSuccess(() -> messageToOperator, true);
-                            return 1;
                         })
                 ));
     }
