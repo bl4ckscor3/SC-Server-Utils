35e98fc5e25bdd9c9d0c3a52596bd182e0367f80
bl4ckscor3 <bl4ckscor33@gmail.com>
2026-07-19 14:37:06 +0200

    Fix spawn protection block bypass not working

--- src/main/java/bl4ckscor3/mod/scserverutils/mixin/ServerGamePacketListenerImplMixin.java
+++ src/main/java/bl4ckscor3/mod/scserverutils/mixin/ServerGamePacketListenerImplMixin.java
@@ -1,30 +1,44 @@
 package bl4ckscor3.mod.scserverutils.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import bl4ckscor3.mod.scserverutils.SpawnProtectionHandler;
 import bl4ckscor3.mod.scserverutils.configuration.Configuration;
 import bl4ckscor3.mod.scserverutils.configuration.spawnprotection.BlockBypass;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 
 @Mixin(ServerGamePacketListenerImpl.class)
 public class ServerGamePacketListenerImplMixin {
-	@Redirect(method = "handleUseItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;mayInteract(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/core/BlockPos;)Z"))
-	private boolean scserverutils$allowInteractionInSpawnProtection(ServerLevel level, Entity entity, BlockPos pos) {
+	@WrapOperation(method = "handleUseItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;isUnderSpawnProtection(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/player/Player;)Z"))
+	private boolean scserverutils$notInSpawnProtectionIfInteractionWithAllowedBlock(MinecraftServer server, ServerLevel level, BlockPos pos, Player player, Operation<Boolean> original) {
+		return !scserverutils$interactsWithAllowedBlock(level, pos) && original.call(server, level, pos, player);
+	}
+
+	@WrapOperation(method = "handleUseItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;mayInteract(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/core/BlockPos;)Z"))
+	private boolean scserverutils$allowInteractionInSpawnProtection(ServerLevel level, Entity entity, BlockPos pos, Operation<Boolean> original) {
+		return scserverutils$interactsWithAllowedBlock(level, pos) || original.call(level, entity, pos);
+	}
+
+	@Unique
+	private static boolean scserverutils$interactsWithAllowedBlock(ServerLevel level, BlockPos pos) {
 		BlockBypass spawnProtectionBlockBypass = Configuration.instance.spawnProtection.blockBypass();
 
 		if (spawnProtectionBlockBypass.enabled().get() && SpawnProtectionHandler.isInSpawnProtection(level, pos)) {
 			String blockId = level.getBlockState(pos).typeHolder().getRegisteredName();
 
-			if (spawnProtectionBlockBypass.blocks().get().contains(blockId))
-				return true;
+			return spawnProtectionBlockBypass.blocks().get().contains(blockId);
 		}
 
-		return level.mayInteract(entity, pos);
+		return false;
 	}
 }
