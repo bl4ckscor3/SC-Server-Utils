c1c928973ef6e4c558e4adfc3d68942262b4f2a0
bl4ckscor3 <bl4ckscor33@gmail.com>
2026-07-19 17:46:16 +0200

    Fix typo in mixin class name

--- src/main/java/bl4ckscor3/mod/scserverutils/mixin/sdlink/SDLinkChatUtilsMixin.java
+++ src/main/java/bl4ckscor3/mod/scserverutils/mixin/sdlink/SDLinkChatUtilsMixin.java
@@ -7,7 +7,7 @@ import com.hypherionmc.sdlink.util.SDLinkChatUtils;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 
 @Mixin(SDLinkChatUtils.class)
-public class SCLinkChatUtilsMixin {
+public class SDLinkChatUtilsMixin {
 	@ModifyReturnValue(method = "applyFiltering", at = @At("TAIL"))
 	private static String scserverutils$filterParagraphSign(String original) {
 		return original.replace("§", "");
